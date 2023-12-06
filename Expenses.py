d4be7a210337c7c634e6d234abf47400317bc3b9
jelizondo <julia.elizondo@endava.com>
2023-12-06 18:33:56 -0300

    refactor Expense class

--- Expenses.py
+++ Expenses.py
@@ -1,5 +1,5 @@
 
 class Expense:
     
-    def __init__(self, name, amount, category) -> None:
-        self.data = {'name': name, 'amount': amount, 'category': category}
+    def __init__(self, data) -> None:
+        self.data = data
